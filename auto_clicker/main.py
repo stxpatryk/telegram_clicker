081b1cdc978e2a770274bfd5bfad4000a2ba114a
Patryk Laskowski <patryk.laskowski@stxnext.pl>
2024-09-06 06:48:19 +0200

    mark click

--- auto_clicker/main.py
+++ auto_clicker/main.py
@@ -5,8 +5,8 @@ import time
 import numpy as np
 from datetime import datetime
 from mss import mss
-
-from typing import Tuple
+import random
+from typing import Tuple, List
 
 ROI = Tuple[int, int, int, int]  # e.g. (2063, 4, 1377, 1314)
 
@@ -54,18 +54,61 @@ def label_image(image: np.array, text: str) -> np.array:
     return img
 
 
+def click_at_coordinates(x, y) -> None:
+    pyautogui.moveTo(x, y)
+    pyautogui.click()
+
+def make_coordinates_absolute(coordinates, roi) -> List[int]:
+    # MAKE IT VISIBLE ON SECON FULL SS IMAGE VIEW before actually "CLICK"
+    # "top": roi[1],  # y-coordinate
+    # "left": roi[0],  # x-coordinate
+    pass
+
+def detect_click_coordinates(image, roi) -> List[List[int]]:
+    # image.shape: (902, 804, 3)
+    height, width, *_ = image.shape
+
+    def get_random(n=10) -> List[List[int]]:
+        click_coordinates = []
+        for _ in range(random.randrange(start=0, stop=n, step=1)):
+            x = random.randrange(start=0, stop=width, step=1)
+            y = random.randrange(start=0, stop=height, step=1)
+            click_coordinates.append([x, y])
+
+        return click_coordinates
+
+    return get_random()
+
+
+def click(coordinates):
+    pass
+
+
+def mark_click(image, coordinates, roi=None) -> np.array:
+    img = np.array(image)
+    x, y = coordinates
+    cv2.circle(img, (x, y), 15, (0, 0, 255), 2)
+
+    return img
+
+
 def main():
-    image = take_full_screenshot()
-    roi = select_roi(image)
+    screenshot = take_full_screenshot()
+    roi = select_roi(screenshot)
 
     while True:
         start_time = time.time()
-        image_roi = take_selection_screenshot(roi)
+        screenshot_roi = take_selection_screenshot(roi)
         elapsed_time = time.time() - start_time
+
+        for coordinates in detect_click_coordinates(screenshot_roi, roi):
+            # click(coordinates)
+            screenshot_roi = mark_click(screenshot_roi, coordinates, roi)
+
 
         text = f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}, FPS: {1/elapsed_time:.2f}"
         print(text)
-        img = label_image(image_roi, text)
+        img = label_image(screenshot_roi, text)
 
         cv2.imshow("Cropped ROI", img)
 
